f380c3a6e7bc92ed7b0010d771e90a5505085026
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-04-24 21:30:39 +0300

    initial commit

---
+++ src/test/java/com/maksimkaxxl/usermanagementapi/UserManagementApiAppTests.java
@@ -0,0 +1,13 @@
+package com.maksimkaxxl.usermanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApiAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
